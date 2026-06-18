00cfcf3fd3b882eecd92a131ee9344f8329d9c0c
Ashish Kumar <ashishkr45943@gmail.com>
2026-06-18 21:26:33 +0530

    streamlined index

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,19 @@
-import dotenv from "dotenv";
-dotenv.config();
-
 import express from "express";
 import cors from "cors";
-import loginRouter from "./routes/auth";
-import contentRouter from "./routes/content";
+import helmet from "helmet";
+import router from "./routes/index.route";
+import { corsUrl } from "./config";
+import connectDB from "./database/db";
 
 const app = express();
-const port = 3000;
 
-// Middlewares
-app.use(cors()); 
 app.use(express.json());
+app.use(helmet());
+app.use(cors({
+  origin: corsUrl
+})); 
 
-// Connecting the routers
-app.use("/api/auth", loginRouter); 
-app.use("/api/content", contentRouter);
+connectDB();
+app.use("/api", router); 
 
-const JWT_SECRET = process.env.JWT_SECRET;
-if (!JWT_SECRET) {
-  throw new Error("JWT_SECRET must be defined in .env file");
-}
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+export default app;
