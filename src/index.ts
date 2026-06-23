bd8ac6d0290eee88504e3cdcd0a537331b0d87d0
Ashish Kumar <ashishkr45943@gmail.com>
2026-06-23 23:01:57 +0530

    feat: added global rate-limit, implemented /health route handler

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,30 @@ import helmet from "helmet";
 import router from "./routes/index.route";
 import { corsUrl } from "./config";
 import connectDB from "./database/db";
+import { errorHandler, notFoundHandler } from "./middleware/errorHandler";
+import { apiLimiter } from "./middleware/rateLimiter";
 
 const app = express();
 
 app.use(express.json());
 app.use(helmet());
-app.use(cors({
-  origin: corsUrl
-})); 
+app.use(cors({ origin: corsUrl })); 
 
 connectDB();
+
+app.use("/api", apiLimiter);
+
 app.use("/api", router); 
+
+app.get("/health", (req, res) => {
+  res.json({
+    success: true,
+    message: "Server is Good",
+    timestamp: new Date().toISOString,
+  })
+});
+
+app.use(notFoundHandler);
+app.use(errorHandler);
 
 export default app;
